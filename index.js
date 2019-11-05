0584c4676a537a86b3b1f130511ea0e30fcf41a5
Aakash Gajjar <skyqutip@gmail.com>
2019-11-05 22:27:06 +0530

    server url

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const path = require('path')
+const request = require('request')
 const PORT = process.env.PORT || 5000
 
+function get(url) {
+    return new Promise((resolve, reject) => {
+        request(url, function (error, response, body) {
+            if (error) {
+                reject(error)
+                return
+            }
+            resolve(body)
+        })
+    })
+}
+
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/app', (req, res) => {
+        get(req.query.url).then((response) => {
+            res.send(response)
+        })
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
