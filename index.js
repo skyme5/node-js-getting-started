baeb3041b9af2c8ef41fba5dc231772ff157f6e0
Aakash Gajjar <skyqutip@gmail.com>
2019-11-05 22:50:55 +0530

    setup

--- index.js
+++ index.js
@@ -1,55 +1,76 @@
-const express = require('express')
-const path = require('path')
-const request = require('request')
-const PORT = process.env.PORT || 5000
-const
-    socks5 = require('simple-socks'),
-    server = socks5.createServer().listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var httpProxy = require("http-proxy");
+var http = require("http");
+var url = require("url");
+var net = require('net');
 
+var server = http.createServer(function (req, res) {
+  var urlObj = url.parse(req.url);
+  var target = urlObj.protocol + "//" + urlObj.host;
 
-// function get(url) {
-//     return new Promise((resolve, reject) => {
-//         request(url, function (error, response, body) {
-//             if (error) {
-//                 reject(error)
-//                 return
-//             }
-//             resolve(body)
-//         })
-//     })
-// }
+  console.log("Proxy HTTP request for:", target);
 
-// express()
-//     .use(express.static(path.join(__dirname, 'public')))
-//     .set('views', path.join(__dirname, 'views'))
-//     .set('view engine', 'ejs')
-//     .get('/app', (req, res) => {
-//         get(req.query.url).then((response) => {
-//             res.send(response)
-//         }).catch((err)=>{
-//             res.send(err)
-//         })
-//     })
-//     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  var proxy = httpProxy.createProxyServer({});
+  proxy.on("error", function (err, req, res) {
+    console.log("proxy error", err);
+    res.end();
+  });
 
-// When a reqest arrives for a remote destination
-server.on('proxyConnect', function (info, destination) {
-    console.log('connected to remote server at %s:%d', info);
-});
+  proxy.web(req, res, {target: target});
+}).listen(8080);  //this is the port your clients will connect to
 
-// When data arrives from the remote connection
-// server.on('proxyData', function (data) {
-//   console.log(data.length);
-// });
+var regex_hostport = /^([^:]+)(:([0-9]+))?$/;
 
-// When an error occurs connecting to remote destination
-server.on('proxyError', function (err) {
-    console.error('unable to connect to remote server');
-    console.error(err);
-});
+var getHostPortFromString = function (hostString, defaultPort) {
+  var host = hostString;
+  var port = defaultPort;
 
-// When a proxy connection ends
-server.on('proxyEnd', function (response, args) {
-    console.log('socket closed with code %d', response);
-    console.log(args);
+  var result = regex_hostport.exec(hostString);
+  if (result != null) {
+    host = result[1];
+    if (result[2] != null) {
+      port = result[3];
+    }
+  }
+
+  return ( [host, port] );
+};
+
+server.addListener('connect', function (req, socket, bodyhead) {
+  var hostPort = getHostPortFromString(req.url, 443);
+  var hostDomain = hostPort[0];
+  var port = parseInt(hostPort[1]);
+  console.log("Proxying HTTPS request for:", hostDomain, port);
+
+  var proxySocket = new net.Socket();
+  proxySocket.connect(port, hostDomain, function () {
+      proxySocket.write(bodyhead);
+      socket.write("HTTP/" + req.httpVersion + " 200 Connection established\r\n\r\n");
+    }
+  );
+
+  proxySocket.on('data', function (chunk) {
+    socket.write(chunk);
+  });
+
+  proxySocket.on('end', function () {
+    socket.end();
+  });
+
+  proxySocket.on('error', function () {
+    socket.write("HTTP/" + req.httpVersion + " 500 Connection error\r\n\r\n");
+    socket.end();
+  });
+
+  socket.on('data', function (chunk) {
+    proxySocket.write(chunk);
+  });
+
+  socket.on('end', function () {
+    proxySocket.end();
+  });
+
+  socket.on('error', function () {
+    proxySocket.end();
+  });
+
 });
